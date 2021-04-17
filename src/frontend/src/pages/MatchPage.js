2aa45c58345f610e475852dc8c6dd9821b204900
Aravind Sontireddy <aravindkumar.sontireddy@gmail.com>
2021-04-17 00:50:31 -0700

    Completed UI elements for team page

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,10 @@
+import { React, useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+
+export const MatchPage = function MatchPage() {
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+        </div>
+    );
+}
